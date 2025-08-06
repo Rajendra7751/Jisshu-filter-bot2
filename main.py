6c09324a2ddb5b30015b42865c5786e46da386a1
Rajendra7751 <rajendraswainrs77@gmail.com>
2025-08-06 09:53:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,18 @@
 import os
-import threading
-import asyncio
-from flask import Flask
 from pyrogram import Client
 from bot.handlers import register_handlers
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.environ["API_ID"])
+API_HASH = os.environ["API_HASH"]
+BOT_TOKEN = os.environ["BOT_TOKEN"]
 
-app = Flask(__name__)
-
-@app.route("/")
-def root():
-    return "Bot is running!"
-
-def start_pyrogram():
-    asyncio.set_event_loop(asyncio.new_event_loop())  # Add this line!
-    client = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, workers=8)
-    register_handlers(client)
-    client.run()
 if __name__ == "__main__":
-    threading.Thread(target=start_pyrogram).start()
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 8080)))
+    bot = Client(
+        "bot",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        bot_token=BOT_TOKEN,
+        workers=8  # You can reduce to 1 if system is low-memory
+    )
+    register_handlers(bot)
+    bot.run()
